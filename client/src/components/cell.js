8b6095129b03b9033e7287427eb851c6c1553ac3
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-04-30 13:59:00 +0600

    Edit Cell component style

--- client/src/components/cell.js
+++ client/src/components/cell.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Tooltip from './tooltip';
 import { getJson } from '../app-lib';
 import { API_METHOD_SERIE_FIGURINE_INFO } from '../constants';
-import Tooltip from './tooltip';
 
 class Cell extends Component {
   constructor(props) {
     super(props);
-    this.state = { serieFigurineInfo: { not_bought_figurine: null, bought_figurine: null } };
+    this.state = {
+      serieFigurineInfo: {
+        not_bought_figurine: [],
+        bought_figurine: [],
+        bought_figurine_count: null,
+        not_bought_figurine_count: null,
+      },
+    };
     getJson(`${API_METHOD_SERIE_FIGURINE_INFO}/${this.props.serie.id}`)
       .then(response => this.setState({ serieFigurineInfo: response }));
   }
@@ -15,19 +22,29 @@ class Cell extends Component {
   render() {
     const { serie } = this.props;
     const info = this.state.serieFigurineInfo;
-    const tooltipText = `Bought figurine: ${info.bought_figurine}. Not bought figurine: ${info.not_bought_figurine}`;
     console.warn(this.state.serieFigurineInfo);
     return (
       <div className="cell" >
-        <div className="cell-content">
-          <Tooltip tooltipText={tooltipText}>
+        <img className="image" src={serie.image} alt="Serie" />
+        <div className="cell-body">
+          <h3 className="cell-header">{serie.name}</h3>
+          <div className="cell-footer">
             <i className="fas fa-chess-queen cell-icon" />
-          </Tooltip>
-          <img className="image" src={serie.image} alt="Serie" />
-          <img className="photo" src={serie.photo} alt="Serie" />
-          <a className="cell-link" href="./#">More</a>
+            <strong>
+              In:
+              <span>
+                <Tooltip tooltipText={info.bought_figurine.join(', ') || '-'}> {info.bought_figurine_count} </Tooltip>
+              </span>
+              | Out:
+              <span>
+                <Tooltip tooltipText={info.not_bought_figurine.join(', ') || '-'}> {info.not_bought_figurine_count}</Tooltip>
+              </span>
+            </strong>
+            <div className="fl-right">
+              <a className="cell-link" href="./#">Read More</a>
+            </div>
+          </div>
         </div>
-        <div className="cell-header">{serie.name}</div>
       </div>
     );
   }
