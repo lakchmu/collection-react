7674fc8cf5b86e84686a84792b0e9270227f92a8
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-05-08 13:51:36 +0600

    Get data for WarningInfo component

--- client/src/components/info.js
+++ client/src/components/info.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
 import SeriesForDecades from './series-for-decades';
 import GeneralInfo from './general-info';
-// import { getJson } from '../app-lib';
-// import { API_METHOD_SERIES } from '../constants';
+import WarningInfo from './warning-info';
 
 class Info extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { series: [] };
-  //   getJson(`${API_METHOD_SERIES}?show_on_the_home=True`)
-  //     .then(response => this.setState({ series: response.results }));
-  // }
-
   render() {
     return (
       <div className="info">
@@ -22,30 +14,7 @@ class Info extends Component {
             alt="diagram"
           />
         </div>
-        <div className="warning-info">
-          <table>
-            <tr>
-              <th>Year</th>
-              <th>Series Title</th>
-              <th>Series Id</th>
-            </tr>
-            <tr>
-              <td>1974</td>
-              <td>Балерины</td>
-              <td>667</td>
-            </tr>
-            <tr>
-              <td>1974</td>
-              <td>Балерины</td>
-              <td>667</td>
-            </tr>
-            <tr>
-              <td>1974</td>
-              <td>Балерины</td>
-              <td>667</td>
-            </tr>
-          </table>
-        </div>
+        <WarningInfo />
         <SeriesForDecades />
       </div>
     );
---
+++ client/src/components/warning-info.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import { getJson } from '../app-lib';
+import { API_METHOD_SERIES } from '../constants';
+
+class WarningInfo extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { series: [] };
+    getJson(`${API_METHOD_SERIES}?figurine__isnull=True`).then(response => this.setState({ series: response.results }));
+  }
+
+  render() {
+    const { series } = this.state;
+    console.warn(series);
+    return (
+      <div className="warning-info">
+        <table>
+          <tr>
+            <th>Year</th>
+            <th>Series Title</th>
+            <th>Series Id</th>
+          </tr>
+          <tbody>
+            {series.map(s => (
+              <tr>
+                <td>{s.year}</td>
+                <td>{s.name}</td>
+                <td>{s.id}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+export default WarningInfo;
