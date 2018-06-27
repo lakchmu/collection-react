e0d0191e35d96a8194cd0530ef19c2f29c52d928
Daria Kurmaeva <d.kurmaeva.speople@gmail.com>
2018-06-27 09:45:45 +0600

    Сhanges in the SeriesForDecades component:
    1) Refactoring the entire code.
    2) Replacing business logic.
    3) Adding new functionality:
    	a. Using Card in the modal window.
    	b. Searching by series.

--- client/src/components/series-for-decades.js
+++ client/src/components/series-for-decades.js
@@ -1,32 +1,89 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 import Modal from './modal';
 import Search from './form/search';
-import { getJson } from '../app-lib';
-import { API_URL, API_METHOD_SERIES_FOR_DECADES } from '../constants';
+import { getJson, deepCopy } from '../app-lib';
+import { API_METHOD_SERIES, API_METHOD_YEARS } from '../constants';
 import Card from './card';
 
 class SeriesForDecades extends Component {
   constructor(props) {
     super(props);
-    this.state = { data: [] };
-    getJson(`${API_METHOD_SERIES_FOR_DECADES}`)
-      .then(response => this.setState({ data: response.data_list }));
+    this.state = {
+      data: [],
+      modalsState: [],
+      years: [],
+      currentYear: null,
+    };
+    this.handleSearch = this.handleSearch.bind(this);
+    this.handleClickBack = this.handleClickBack.bind(this);
+    this.toolBar = React.createRef();
+    this.modalAnchor = React.createRef();
+    getJson(`${API_METHOD_YEARS}`)
+      .then(response => this.setState({ years: response.data_list }));
+  }
+
+  handleClickCell(year) {
+    getJson(`${API_METHOD_SERIES}?year=${year}&status=bought`)
+      .then((response) => {
+        this.setState({
+          data: response.results,
+          modalsState: deepCopy(response.results),
+          currentYear: year,
+        });
+        this.modalAnchor.current.click();
+      });
+  }
+
+  handleSearch(event) {
+    event.preventDefault();
+    const searchText = event.target.search.value;
+    getJson(`${API_METHOD_SERIES}?year=${this.state.currentYear}&status=bought&search=${searchText}`)
+      .then((response) => {
+        this.setState({ modalsState: response.results });
+        this.toolBar.current.classList.add('show');
+      });
+  }
+
+  handleClickBack() {
+    const { data } = this.state;
+    this.setState({ modalsState: deepCopy(data) });
+    this.toolBar.current.classList.remove('show');
   }
 
   render() {
-    const { data } = this.state;
-    const listModal = data.map(item => (
-      <Modal anchor={<div className="cell">{item.year}</div>} key={item.year}>
-        <div className="modal-title">{item.year}</div>
-        <Search />
-        <div className="modal-body">
-          {item.series.map(serie => <Card serie={serie} key={serie.id} apiUrl={API_URL} />)}
-        </div>
-      </Modal>
+    const { years, modalsState } = this.state;
+    const SearchWithRouter = withRouter(Search);
+    const listCell = years.map(year => (
+      <button
+        className="cell"
+        onClick={() => this.handleClickCell(year)}
+        key={year}
+      >{year}
+      </button>
     ));
     return (
       <div className="series-for-decades">
-        { listModal }
+        { listCell }
+        <Modal anchor={<span ref={this.modalAnchor} />}>
+          <div className="modal-title">{this.state.currentYear}</div>
+          <SearchWithRouter handleSearch={e => this.handleSearch(e)} />
+          <div className="modal-body">
+            <div className="row tool-bar" ref={this.toolBar}>
+              <div className="col-12">
+                <button
+                  className="button button-reverse"
+                  type="button"
+                  onClick={() => this.handleClickBack()}
+                ><i className="fas fa-arrow-circle-left" />Back
+                </button>
+              </div>
+            </div>
+            {modalsState.map(serie => (
+              <Card serie={serie} key={serie.id} />
+            ))}
+          </div>
+        </Modal>
       </div>
     );
   }
